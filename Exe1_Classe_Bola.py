f7ce20697bc5320086455f86ea75fc43d5a63173
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-11 13:11:57 -0300

    Update Exe1_Classe_Bola.py

--- Exe1_Classe_Bola.py
+++ Exe1_Classe_Bola.py
@@ -8,4 +8,25 @@ Métodos: trocaCor e mostraCor
 import math
 
 class Bola:
-    def __init__(self,Cor,circunferência,material) -> None:
+    def __init__(self,Cor,raio):
+        self.cor = Cor
+        self.raio = raio
+    
+    def trocaCor(self,cor2):
+        self.cor = cor2
+
+    def mostraCor(self):
+        print(f"Cor: {self.cor}")
+
+    def Area(self):
+        return 4 * math.pi * math.pow(self.raio,2)
+
+
+cor = input("Qual a cor da bola?: ")
+raio = int(input("Digite o raio: "))
+bola = Bola(cor,raio)
+Bola.mostraCor()
+cor2 = input("Digite a cor para trocar: ")
+Bola.trocaCor(cor2)
+Bola.mostraCor()
+print(f"Area: {Bola.Area}")
