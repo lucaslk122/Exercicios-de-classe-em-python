e84c2b7b2003185b3d61aaf0dbd0fc3ef386f96d
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-13 10:38:55 -0300

    Update Exe4_Classe_Pessoa.py

--- Exe4_Classe_Pessoa.py
+++ Exe4_Classe_Pessoa.py
@@ -4,3 +4,39 @@ Atributos: nome, idade, peso e altura
 Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
 """
 
+class Pessoa:
+    def __init__(self, nome: str, idade: int, peso: float, altura: float):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self):
+        if self.idade < 21:
+            self.altura += 0.5
+
+        self.idade += 1
+
+    def engordar(self, peso):
+        self.peso += peso
+
+    def emagrecer(self, peso):
+        self.peso -= peso
+
+    def crescer(self, altura):
+        self.altura += altura
+
+
+    def mostraPessoa(self):
+        print(f'Nome: {self.nome}, idade: {self.idade} anos, peso: {self.peso}kg, altura: {self.altura}cm')
+
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+peso = float(input("Digite o peso: "))
+altura = float(input("Digite a altura: "))
+Pessoa = Pessoa(nome, idade, peso, altura)
+Pessoa.mostraPessoa()
+Pessoa.envelhecer()
+Pessoa.emagrecer(3)
+Pessoa.mostraPessoa()
