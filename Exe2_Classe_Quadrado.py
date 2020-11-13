f4ba65859818183ac78224337d5278e8c4f7bc43
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-13 10:11:48 -0300

    Update Exe2_Classe_Quadrado.py

--- Exe2_Classe_Quadrado.py
+++ Exe2_Classe_Quadrado.py
@@ -9,17 +9,24 @@ class Quadrado:
         self.lado = lado
     
     def MudaLado(self,lado):
-        lado_novo = self.lado
-        return lado_novo
+        self.lado = lado
+        
 
     def Area(self,lado = 0, lado_novo = 0):
-        return self.lado * self.lado
+        return self.lado * self.lado or lado_novo * lado_novo
         
 
 def main():
     lado = int(input("Digite um valor para o lado do quadrado: "))
     quadrado = Quadrado(lado)
     print(f"Area do quadrado: {quadrado.Area(lado)}m^2")
+    troca = input("Dseja trocar o valor do lado? s/n: ").lower()
+    if troca == "s":
+        lado_novo = int(input("Digite o novo valor: "))
+        quadrado.MudaLado(lado_novo)
+        print((f"A nova area é {quadrado.Area(lado_novo)}m^2"))
+    else:
+        print("A area continua a mesma")
     
 main()
 
