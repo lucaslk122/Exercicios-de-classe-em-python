6e7921186deec250658f894009210b0a27cf8c50
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-16 13:39:45 -0300

    Update Exe5_Classe_Conta_Corrente.py

--- Exe5_Classe_Conta_Corrente.py
+++ Exe5_Classe_Conta_Corrente.py
@@ -1,32 +1,34 @@
 """
 Classe Conta Corrente: Crie uma classe para implementar uma conta corrente. A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo. Os métodos são os seguintes: alterarNome, depósito e saque; No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
 """
-
 class ContaCorrente:
-
-   def __init__(self, nome,numero_conta, saldo = 0):
+    def __init__(self, nome,numero_conta, saldo = 0):
         self.nome = nome
         self.saldo = saldo
         self.numero_conta = numero_conta
-        
+
     def alterarNome(self, nome):
         self.nome = nome
+        return self.nome
 
-    def depósito(self, valor):
-        self.saldo = self,valor + valor
+    def depósito(self, deposito):
+        self.saldo += deposito
+        return self.saldo
 
-    def saque(self, valor):
-        self.saldo = self.saldo - valor
+    def saque(self,saque):
+        self.saldo -= saque
+        return self.saldo
 
 nome = input("Digite seu nome: ")
 numero_conta = int(input("Digite o numero da conta: "))
-saldo = float(input("Digite o saldo: "))
-deposito = float(input("Digite o valor do deposito: "))
-conta = ContaCorrente(nome,saldo, numero_conta,deposito)
-resposta = int("Dseja trocar de nome? s/n: ").lower()
+conta = ContaCorrente(nome, numero_conta)
+resposta = input("Dseja trocar de nome? s/n: ").lower()
 if resposta == "s":
     novo_nome = input("Digite o novo nome: ") 
     conta.alterarNome(novo_nome)
 else:
     pass
-print()
+deposito = float(input("Digite o valor do deposito: "))
+conta.depósito(deposito)
+saque = float(input("Digite o valor do saque: "))
+conta.saque(saque)
