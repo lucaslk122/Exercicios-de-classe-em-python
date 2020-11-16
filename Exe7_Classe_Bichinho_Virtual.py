e5a8b7e590ee033750a0a54b518c50546b460ad7
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-16 16:16:43 -0300

    Create Exe7_Classe_Bichinho_Virtual.py

---
+++ Exe7_Classe_Bichinho_Virtual.py
@@ -0,0 +1,58 @@
+"""
+Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
+
+Atributos: Nome, Fome, Saúde e Idade b. Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
+"""
+
+from io import TextIOBase
+
+
+class BichinhoVirtual:
+
+    def __init__(self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def MudaNome (self,nome):
+        self.nome = nome
+
+    def MudaFome(self, fome):
+        self.fome = fome
+
+    def MudaSaude(self, saude):
+        self.saude = saude
+
+    def MudaIdade(self,idade):
+        self.idade = idade
+
+    def Humor(self, Fome, saude):
+        return fome * saude
+
+nome = input("Digite o nome do seu tamaguchi: ")
+fome = int(input("Digite a fome: "))
+saude = int(input("Digite a saude: "))
+idade = int(input("Digite a idade: "))
+tamaguchi = BichinhoVirtual(nome, fome, saude, idade)
+print("-"*30 + "Seu tamaguchi (atual) " + "-"*30)
+print(f"Nome: {tamaguchi.nome}")
+print(f"Fome: {tamaguchi.fome}")
+print(f"Saúde: {tamaguchi.saude}")
+print(f"Idade: {tamaguchi.idade}")
+print(f"Humor: {tamaguchi.Humor(fome, saude)}")
+resposta = input("Deseja mudar seus parametros? (nome, fome, saude e idade)? s/n: ").lower()
+if resposta == "n":
+    pass
+else:
+    nome = input("Digite o nome do seu tamaguchi: ")
+    fome = int(input("Digite a fome: "))
+    saude = int(input("Digite a saude: "))
+    idade = int(input("Digite a idade: "))
+    tamaguchi = BichinhoVirtual(nome, fome, saude, idade)
+    print("-"*30 + "Seu tamaguchi (atual) " + "-"*30)
+    print(f"Nome: {tamaguchi.MudaNome}")
+    print(f"Fome: {tamaguchi.MudaFome}")
+    print(f"Saúde: {tamaguchi.MudaSaude}")
+    print(f"Idade: {tamaguchi.MudaIdade}")
+    print(f"Humor: {tamaguchi.Humor(fome, saude)}")
