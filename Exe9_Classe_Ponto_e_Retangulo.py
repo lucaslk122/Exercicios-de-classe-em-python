0842f15378eb16777f1a6e2e4c532b6b4e9aeaf0
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-17 09:44:20 -0300

    Update Exe9_Classe_Ponto_e_Retangulo.py

--- Exe9_Classe_Ponto_e_Retangulo.py
+++ Exe9_Classe_Ponto_e_Retangulo.py
@@ -12,3 +12,30 @@ O valor do centro do objeto deve ser mostrado na tela
 Crie um menu para alterar os valores do retângulo e imprimir o centro deste retângulo.
 """
 
+class Ponto:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+class Retangulo:
+    def __init__(self,ladoA, ladoB):
+        self.ladoA = ladoA
+        self.ladoB = ladoB
+    
+    def Centro(self):
+        x_centro = (self.ladoA.x + self.ladoB.x) / 2
+        y_centro = (self.ladoA.y + self.ladoB.y) / 2
+        return f"X = {x_centro}"
+        return f"Y = {y_centro}"
+
+def Main():
+    x1 = float(input("Digite a coordenada x do canto inferior esquerdo: "))
+    y1 = float(input("Digite a coordenada y do canto inferior esquerdo: "))
+    ladoA = Ponto(x1,y1)
+    x2 = float(input("Digite a coordenada x do canto superior direito: "))
+    y2 = float(input("Digite a coordenada y do canto superior direito: "))
+    ladoB = Ponto(x2,y2)
+    ret = Retangulo(ladoA, ladoB)
+    print(f"Ponto central: {ret.Centro()}")
+
+Main()
