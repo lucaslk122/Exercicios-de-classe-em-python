620c4a1bc1512027d2ee5f8200ffb8542b32030a
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-19 09:51:06 -0300

    Update Exe17_classe_bichinhovirtual_melhorado.py

--- Exe17_classe_bichinhovirtual_melhorado.py
+++ Exe17_classe_bichinhovirtual_melhorado.py
@@ -10,31 +10,31 @@ class BichinhoVirtual:
         self.setNome(nome)
         self.setfome(fome)
         self.setsaude(saude)
-        self.setidade(idade)
+        self.setIdade(idade)
 
     def setNome(self, nome):
-        self.nome = nome
-
-    def setfome (self, fome):
-        self.setfome = fome
-
-    def setidade (self, idade):
-        self.idade = idade
-
-    def setSaude (self, saude):
+        self.nome = nome        
+        
+    def setFome(self, fome):
+        self.fome = fome
+    
+    def setSaude(self, saude):
         self.saude = saude
-
+    
+    def setIdade(self, idade):
+        self.idade = idade
+    
     def getNome(self):
         return self.nome
     
     def getFome(self):
-        return self.getFome
-
+        return self.fome
+    
     def getSaude(self):
-        return self.getSaude
-
+        return self.saude
+    
     def getIdade(self):
-        return self.getIdade
+        return self.idade
 
     def Humor(self):
         return self.getFome() * self.getSaude()
@@ -50,18 +50,34 @@ class BichinhoVirtual:
     def str(self):
         return (f"Nome: {self.getNome}, Fome: {self.getFome}, saúde: {self.getSaude}, idade: {self.getIdade}")
 
-nome = input("Digite o nome do seu bichinho: ")
-idade = int(input("Digite a idade: "))
-bicho1 = BichinhoVirtual(nome, random.randint(0,10), random.randint(0,10),idade)
-nome1 = input("Digite o noem do bichinho 2: ")
-idade1 = int(input("Digite a idade: "))
-bicho2 = BichinhoVirtual(nome1, random.randint(0,10), random.randint(0,10),idade1)
-nome2 = input("Digite o nome do bicnho 3: ")
-idade2 = int(input("Digite a idade: "))
-bicho3 = BichinhoVirtual(nome2, random.randint(0,10), random.randint(0,10),idade2)
+
+bicho1 = BichinhoVirtual("lucas", random.randint(0,10), random.randint(0,10),25)
+bicho2 = BichinhoVirtual("Sarah", random.randint(0,10), random.randint(0,10),8)
+bicho3 = BichinhoVirtual("Pedro", random.randint(0,10), random.randint(0,10),9)
 fazenda = []
 fazenda.append(bicho1)
 fazenda.append(bicho2)
 fazenda.append(bicho3)
-print(vars(fazenda))
+
+while True:
+    print("-"*20 + "Fazenda" + "-"*20)
+    print("1. Alimentar todos os bichos")
+    print("2. Brincar com todos os bichos")
+    print("3. Ouvir todos os bichos")
+    print("4. Sair")
+    op = int(input("Digite a opção: "))
+    
+    if (op == 1):
+        alimento = int(input("Alimentar todos com: "))
+        for i in range(3):
+            fazenda[i].alimenta(alimento)
+    elif(op ==2):
+        brinquedo = int(input("Brincar todos com: "))
+        for i in range(3):
+            fazenda[i].brincar(brinquedo)
+    elif(op == 3):
+        for i in range(3):
+            print(fazenda[i].getNome() + ": " + str(fazenda[i].humor()))
+    elif(op == 4):
+        break
 
